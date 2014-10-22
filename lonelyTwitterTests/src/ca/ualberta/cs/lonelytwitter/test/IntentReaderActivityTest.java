610a0e61bcd4ced608a564f1099ded8f8a18dca7
jdemeryx <jdemery@ualberta.ca>
2014-10-21 18:07:46 -0600

    submission for Lab6

--- lonelyTwitterTests/src/ca/ualberta/cs/lonelytwitter/test/IntentReaderActivityTest.java
+++ lonelyTwitterTests/src/ca/ualberta/cs/lonelytwitter/test/IntentReaderActivityTest.java
@@ -16,5 +16,72 @@ public class IntentReaderActivityTest extends
     }
 
 //TODO: Add your code here:
-       
+    public void testSendText() {
+    	Intent intent = new Intent();
+    	intent.putExtra(IntentReaderActivity.TEXT_TO_TRANSFORM_KEY, "testing send");
+    	
+    	setActivityIntent(intent);
+    	
+    	IntentReaderActivity ira = getActivity();
+    	assertEquals(ira.getText(), "testing send");
+    }
+    
+    public void testDisplayText() {
+    	Intent intent = new Intent();
+    	intent.putExtra(IntentReaderActivity.TEXT_TO_TRANSFORM_KEY, "testing display text");
+    	
+    	setActivityIntent(intent);
+    	
+    	IntentReaderActivity ira = getActivity();
+    	TextView textView = 
+    			(TextView)ira.findViewById(ca.ualberta.cs.lonelytwitter.R.id.intentText);
+    	assertEquals(textView.getText(), "testing display text");
+    }
+    
+    public void testDoubleText() {
+    	Intent intent = new Intent();
+    	intent.putExtra(IntentReaderActivity.TEXT_TO_TRANSFORM_KEY, "testing double text");
+    	intent.putExtra(IntentReaderActivity.MODE_OF_TRANSFORM_KEY, IntentReaderActivity.DOUBLE);
+    	
+    	setActivityIntent(intent);
+    	
+    	IntentReaderActivity ira = getActivity();
+    	
+    	assertEquals(ira.getText(), "testing double texttesting double text");
+    }
+    
+    public void testReverseText() {
+    	Intent intent = new Intent();
+    	intent.putExtra(IntentReaderActivity.TEXT_TO_TRANSFORM_KEY, "testing reverse text");
+    	intent.putExtra(IntentReaderActivity.MODE_OF_TRANSFORM_KEY, IntentReaderActivity.REVERSE);
+    	
+    	setActivityIntent(intent);
+    	
+    	IntentReaderActivity ira = getActivity();
+
+    	assertEquals(ira.getText(), "txet esrever gnitset");
+    }
+    
+    public void testDefaultMessage() {
+    	Intent intent = new Intent();
+    	
+    	setActivityIntent(intent);
+    	
+    	IntentReaderActivity ira = getActivity();
+    	
+    	assertEquals(ira.getText(), "Default Text");
+    }
+    
+    public void testTextView() {
+    	Intent intent = new Intent();
+    	
+    	setActivityIntent(intent);
+    	
+    	IntentReaderActivity ira = getActivity();
+    	TextView test = (TextView)ira.findViewById(
+    			ca.ualberta.cs.lonelytwitter.R.id.intentText);
+    	TextView test2 = (TextView)ira.getWindow().getDecorView().findViewById(
+    			ca.ualberta.cs.lonelytwitter.R.id.intentText);
+    	ViewAsserts.assertOnScreen(test, test2);
+    }
 }
